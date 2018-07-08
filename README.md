0b57edd2ae50bb26503f4afd4b761c8882dc5758
lgc <lgc@lgc.com>
2018-07-08 19:52:20 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+php7.0环境，支持redis,mongodb,solr,swoole扩展
+cron配置参考
+environment:
+   - CRON=*/5 * * * * /usr/share/nginx/html/yii chat/index
---
+++ laravoole/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+#初始化工作环境
+chmod -R 777 ${LARAVOOLE_WORKER_DIR}/bootstrap/cache
+chmod -R 777 ${LARAVOOLE_WORKER_DIR}/storage/logs
+
+# 启动php larvoole 进程
+php ${LARAVOOLE_WORKER_DIR}/artisan laravoole start
