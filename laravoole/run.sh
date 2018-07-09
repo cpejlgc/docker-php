d22c4a9a4c335f730d26169f69b4187245938fe8
lgc <lgc@lgc.com>
2018-07-09 11:53:37 +0800

    laravoole run.sh

--- laravoole/run.sh
+++ laravoole/run.sh
@@ -3,7 +3,7 @@
 
 #初始化工作环境
 chmod -R 777 ${LARAVOOLE_WORKER_DIR}/bootstrap/cache
-chmod -R 777 ${LARAVOOLE_WORKER_DIR}/storage/logs
+chmod -R 777 ${LARAVOOLE_WORKER_DIR}/storage
 
 # 启动php larvoole 进程
 php ${LARAVOOLE_WORKER_DIR}/artisan laravoole start
