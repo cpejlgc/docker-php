3dc7da159e76746990e477bb1cc012deed41ecd5
lgc <lgc@lgc.com>
2018-07-10 15:44:09 +0800

    nginx

--- laravoole/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-#初始化工作环境
-chmod -R 777 ${LARAVOOLE_WORKER_DIR}/bootstrap/cache
-chmod -R 777 ${LARAVOOLE_WORKER_DIR}/storage
-
-# 启动php larvoole 进程
-php ${LARAVOOLE_WORKER_DIR}/artisan laravoole start
